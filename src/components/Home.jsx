753aa1e1f0cb9c90abe26c642b9fd7498657f67b
AKASH021001-0202 <nakashn007@gmail.com>
2024-05-13 18:56:20 +0530

    second

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
 const Home = () => {
   const [users, setUsers] = useState([]);
   const [formData, setFormData] = useState({
@@ -82,9 +81,14 @@ const Home = () => {
     if (editMode) {
       // Update existing user
       axios
-        .put(`https://jsonplaceholder.typicode.com/users/${editingUserId}`, formData)
+        .put(
+          `https://jsonplaceholder.typicode.com/users/${editingUserId}`,
+          formData
+        )
         .then((res) => {
-          setUsers(users.map(user => user.id === editingUserId ? formData : user));
+          setUsers(
+            users.map((user) => (user.id === editingUserId ? formData : user))
+          );
           setFormData({
             name: "",
             username: "",
@@ -157,7 +161,7 @@ const Home = () => {
   return (
     <div className="container p-5">
       <form onSubmit={handleSubmit}>
-      <div className="form-group">
+        <div className="form-group">
           <label>Name:</label>
           <input
             type="text"
@@ -297,7 +301,10 @@ const Home = () => {
             onChange={handleChange}
           />
         </div>
-        <button type="submit" className={`btn ${editMode ? 'btn-primary' : 'btn-success'}`}>
+        <button
+          type="submit"
+          className={`btn ${editMode ? "btn-primary" : "btn-success"}`}
+        >
           {editMode ? "Update" : "Submit"}
         </button>
       </form>
@@ -308,82 +315,86 @@ const Home = () => {
         {users.map((user) => (
           <div className="  col-md-12" key={user.id}>
             <div className="card1">
-            <table className="table">
-              <tbody>
-              <tr>
-                  <td>ID:</td>
-                  <td>{user.id}</td>
-                </tr>
-                <tr>
-                  <td>Name:</td>
-                  <td>{user.name}</td>
-                </tr>
-                <tr>
-                  <td>Username:</td>
-                  <td>{user.username}</td>
-                </tr>
-                <tr>
-                  <td>Email:</td>
-                  <td>{user.email}</td>
-                </tr>
-                <tr>
-                  <td>Address:</td>
-                  <td>{user.address.street}</td>
-                </tr>
-                <tr>
-                  <td>suite:</td>
-                  <td>{user.address.suite}</td>
-                </tr>
-                <tr>
-                  <td>city:</td>
-                  <td>{user.address.city}</td>
-                </tr>
-                <tr>
-                  <td>BS:</td>
-                  <td>{user.company.bs}</td>
-                </tr>
-                <tr>
-                  <td>zipcode:</td>
-                  <td>{user.address.zipcode}</td>
-                </tr>
-                <tr>
-                  <td>Geo:lat</td>
-                  <td>{user.address.geo.lat}</td>
-                </tr>
-                <tr>
-                  <td>Geo:lng</td>
-                  <td>{user.address.geo.lng}</td>
-                </tr>
-                <tr>
-                  <td>Phone:</td>
-                  <td>{user.phone}</td>
-                </tr>
-                <tr>
-                  <td>Website:</td>
-                  <td>{user.website}</td>
-                </tr>
-                <tr>
-                  <td>Company:</td>
-                  <td>{user.company.name}</td>
-                </tr>
-                <tr>
-                  <td>Catchphrase:</td>
-                  <td>{user.company.catchPhrase}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <button className="btn btn-danger" onClick={() => deleteUser(user.id)}>
-                      Delete
-                    </button>
-                    <button className="btn btn-primary" onClick={() => handleEdit(user)}>
-                      Edit
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <td>ID:</td>
+                    <td>{user.id}</td>
+                  </tr>
+                  <tr>
+                    <td>Name:</td>
+                    <td>{user.name}</td>
+                  </tr>
+                  <tr>
+                    <td>Username:</td>
+                    <td>{user.username}</td>
+                  </tr>
+                  <tr>
+                    <td>Email:</td>
+                    <td>{user.email}</td>
+                  </tr>
+                  <tr>
+                    <td>Address:</td>
+                    <td>{user.address.street}</td>
+                  </tr>
+                  <tr>
+                    <td>suite:</td>
+                    <td>{user.address.suite}</td>
+                  </tr>
+                  <tr>
+                    <td>city:</td>
+                    <td>{user.address.city}</td>
+                  </tr>
+                  <tr>
+                    <td>BS:</td>
+                    <td>{user.company.bs}</td>
+                  </tr>
+                  <tr>
+                    <td>zipcode:</td>
+                    <td>{user.address.zipcode}</td>
+                  </tr>
+                  <tr>
+                    <td>Geo:lat</td>
+                    <td>{user.address.geo.lat}</td>
+                  </tr>
+                  <tr>
+                    <td>Geo:lng</td>
+                    <td>{user.address.geo.lng}</td>
+                  </tr>
+                  <tr>
+                    <td>Phone:</td>
+                    <td>{user.phone}</td>
+                  </tr>
+                  <tr>
+                    <td>Website:</td>
+                    <td>{user.website}</td>
+                  </tr>
+                  <tr>
+                    <td>Company:</td>
+                    <td>{user.company.name}</td>
+                  </tr>
+                  <tr>
+                    <td>Catchphrase:</td>
+                    <td>{user.company.catchPhrase}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="update-card">
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteUser(user.id)}
+              >
+                Delete
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => handleEdit(user)}
+              >
+                Edit
+              </button>
+              </div>
+              
             </div>
-           
           </div>
         ))}
       </div>
